9724ef6eebd8fb29d0102c56990e554764e2b70d
TanushaTumpala05 <tanushatumpala2005@gmail.com>
2025-10-25 10:25:38 +0530

    Add files via upload
    python Datatypes(code)

---
+++ Datatypes.py
@@ -0,0 +1,124 @@
+'''DATA TYPES'''
+'''INT'''
+# j=20
+# print(j)
+# print(type(j))
+
+# google=1234
+# print(type(google))
+
+# hello=-900
+# print(type(hello))
+
+# h=23455
+# print(h)
+
+# k=2345678899
+# print(type(k))
+
+'''FLOAT'''
+# O=23.345
+# print(O)
+# print(type(O))
+
+# h=-34578.4556
+# print(h)
+# print(type(h))
+
+# Hello=1234.056
+# print(type(Hello))
+
+'''COMPLEX'''
+# a=4+5j
+# print(a)
+
+# hello=6+7j
+# print(hello)
+# print(type(hello))
+
+# v=23+56j
+# print(type(v))
+
+'''TYPE CONVERSION'''
+'''IMPLICIT TYPE CONVERSION'''
+# d=2345
+# print(float(d))
+
+'''EXPLICIT TYPE CONVERSIN'''
+# d=3456.789
+# print(int(d))
+
+'''How to assign multiple values in variables'''
+# x,y,z=12,20,30
+# print(x)
+# print(y)
+# print(z)
+
+'''List'''
+# listname=["hi","hello","how are you"]
+# print(listname)
+
+# x=y=z="Sunny"
+# print(x)
+# print(y)
+# print(z)
+
+# x,y,z="How","are","you"
+# print(x,y,z)
+
+'''SEQUENCE DATA TYPES'''
+'''LIST'''
+# mylist=[1,2,"hi","hello",23.45,3+6j]
+# print(mylist)
+# print(type(mylist))
+
+'''STRING'''
+# name="satya"
+# print (name)
+# print(type(name))
+
+# a="Janu"
+# print(a)
+
+# b=1234
+# print(b)
+# print(type(b))
+
+# h='python'
+# print(h)
+# print(type(h))
+
+'''TUPLE'''
+# e=(1,2,34.89,"hello","welcome")
+# print(e)
+# print(type(e))
+
+# satya=("hello","mygirl",1,2.3,(6+7j),[1,2,3,"hi"])
+# print(satya)
+# print(type(satya))
+
+'''BOOLEAN DATA TYPES'''
+# A=True
+# print(A)
+# print(type(A))
+
+# B=True
+# print(B)
+# print(type(B))
+
+'''SET DATA TYPES'''
+# myset={1,2.3,3+6j,[1,2,3],"hello",(4,6,23)}
+# print(myset)
+# print(type(myset))
+
+# a={}
+# print(type(a))
+
+# abc={1,2,3,"hi","hello"}
+# print(abc)
+# print(type(abc))
+
+'''DICTIONARY DATA TYPES'''
+# abc={"name":"sunny","country":"canada"}
+# print(abc)
+# print(type(abc))
