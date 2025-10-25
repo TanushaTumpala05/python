bc75a0da38fea5b703a0c13519e49598f830d17a
TanushaTumpala05 <tanushatumpala2005@gmail.com>
2025-10-25 10:24:10 +0530

    Add files via upload
    python operators (code)

---
+++ operators.py
@@ -0,0 +1,51 @@
+'''OPERATORS'''
+'''Arthimetic operators'''
+#  a=int(input("enter a number: "))
+# b = int(input("enter a number: "))
+# print("The addition of",a+b)
+# print("The subtraction of ",a-b)
+# print("The multiplication of",a*b)
+# print("The division of",a/b)
+# print("The floor division of",a//b)
+# print("The modular division of",a%b)
+# print("The power of",a**b)
+
+'''Logical operators'''
+# a = 23
+# b = 30
+# c = a<b and b>a 
+# print(c)
+# d = 40
+# e = 45
+# f = d>e or e>d 
+# print(f)
+# a = 1
+# print(not(a))
+# a = 0
+# print(not(a)) 
+
+'''Relational operators'''
+# a = int(input("enter a number: "))
+# b = int(input("enter a number: "))
+# print("The greater value", a>b)
+# print("The lesser value", a<b)
+# print("The greater than and equals too value",a>=b)
+# print("The lesse than and equals too value",a<=b)
+# print("the equals too value",a==b)
+# print("the not equals too value",a!=b)
+
+'''Membership operators'''
+# x = ["apple", "banana", "jack", "kiwi"]
+# print("pineapple" in x) 
+# print("apple" in x)
+# print("dragon" not in x)
+# print("banana" not in x)
+
+'''Identity operators'''
+# x = [ 1,2,3]
+# y = [4,5,6]
+# z =x
+# print(x is z)
+# print(x is y)
+# print(x is not y)
+# print(z is not y)
