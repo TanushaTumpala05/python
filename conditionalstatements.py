a275034d8e13c39e10b55358ef2c34cf20f18c85
TanushaTumpala05 <tanushatumpala2005@gmail.com>
2025-10-25 10:27:25 +0530

    Add files via upload
    python conditional statements(code)

---
+++ conditionalstatements.py
@@ -0,0 +1,43 @@
+'''CONDITIONAL STATEMENTS'''
+'''If'''
+# a=5
+# if a==5:
+#     print("The if condition executed")
+
+'''If Else'''
+# a=10
+# b=15
+# if a!=b and b>15:
+#     print("If block is executed")
+# else:
+#     print("Else block is executed")   
+
+'''Elif'''
+# name=input("Enter the name")
+# userid=int(input("Enter the useridid..."))
+# password=input("Enter the password") 
+# if userid==12345 and password=="ists@123":
+#     print("The if block is executed")
+# elif name=="cse" and password=="cse@123":
+#     print("The elif block is executed"):
+# else:
+#     print("Else block is executed")
+
+'''Nested If'''  
+# a=29
+# b=46
+# c=56
+# d=49
+# if a==29 and b==46:
+#     print("Outer if executed")
+#     if c==56:
+#         print("Inner if executed")
+#     else:
+#         print("Inner else executed")  
+# else:
+#     print("Outer else executed")             
+
+'''Shorthand If'''
+# a=5
+# if a==5:
+#     print("If is executed")   
